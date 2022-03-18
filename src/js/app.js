ea9a14ea5921ce9dc529e5e78137572b062f782a
Andrey Stseba <scebaandru@gmail.com>
2022-03-18 17:38:15 +0200

    creading sections: brands, slider, shop

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,6 @@
 import * as flsFunctions from './modules/functions.js';
-// import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, { Navigation, Pagination } from 'swiper';
+import * as sliders from './modules/sliders.js';
+
 // const swiper = new Swiper();
 flsFunctions.isWebp();
