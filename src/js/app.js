1637e52c234a0e02792f60a56deb047cee7dac49
Andrey Stseba <scebaandru@gmail.com>
2022-03-19 18:15:29 +0200

    creating shop, subscribe, footer.

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 import * as flsFunctions from './modules/functions.js';
 import Swiper, { Navigation, Pagination } from 'swiper';
 import * as sliders from './modules/sliders.js';
+import * as regular from './modules/regular.js';
+import * as forms from './modules/forms.js';
 
 // const swiper = new Swiper();
 flsFunctions.isWebp();
