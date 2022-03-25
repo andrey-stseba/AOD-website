265b8ab80778bc82055338a7de338ef7af8441dc
Andrey Stseba <scebaandru@gmail.com>
2022-03-25 12:31:35 +0200

    refactoring project

--- src/js/app.js
+++
@@ -1,8 +0,0 @@
-import * as flsFunctions from './modules/functions.js';
-// import Swiper, { Navigation, Pagination } from 'swiper';
-import * as sliders from './modules/sliders.js';
-import * as regular from './modules/regular.js';
-import * as forms from './modules/forms.js';
-
-// const swiper = new Swiper();
-flsFunctions.isWebp();
